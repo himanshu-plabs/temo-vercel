83e632f8e9dc52b38e6e7de40d5defd167ec6b87
himanshu-plabs <h@plotlabs.com>
2024-05-28 03:16:41 +0530

    Bug fixes

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -3,103 +3,48 @@ import Link from "next/link";
 import { list } from "@vercel/blob";
 
 // Reusable components
-interface MenuItemProps {
-  title: string;
-  icon: string;
-  description: string;
-}
 
-const MenuItem: React.FC<MenuItemProps> = ({ title, icon, description }) => {
+const MenuSection = ({ title, isSelected = true, items }: any) => {
   return (
-    <div className="border border-gray-300 border-opacity-80 bg-white hover:bg-gray-50 text-gray-600 cursor-pointer group flex items-center gap-2 w-full pl-2 pr-3 py-2 rounded-md ">
-      <div
-        className="w-9 h-9 rounded-md overflow-hidden border bg-gray-100"
-        style={{ flexShrink: 0 }}
-      >
-        {/* <img
-          alt="Demo Thumbnail"
-          loading="lazy"
-          width={34}
-          height={34}
-          decoding="async"
-          data-nimg="1"
-          className="w-full h-full object-cover"
-          src={icon}
-        /> */}
-      </div>
-      <div className="text-sm line-clamp-2">{description}</div>
-    </div>
-  );
-};
-
-interface MenuSectionProps {
-  title: string;
-  items: MenuItemProps[];
-}
-
-const MenuSection: React.FC<MenuSectionProps> = ({ title, items }) => {
-  return (
-    <div className="flex flex-col gap-3 mb-2">
-      <div className="flex items-center justify-between group">
-        <div className="flex gap-2 items-center w-full">
-          <p className="font-semibold text-sm outline-none w-full text-gray-700">
-            {title}
-          </p>
-        </div>
-      </div>
-      {/* <div className="flex flex-col gap-2">
-        {items.map((item, index) => (
-          <div key={index} className={index === items.length - 1 ? "mb-6" : ""}>
-            <MenuItem
-              title={item.title}
-              icon={item.icon}
-              description={item.description}
-            />
-          </div>
-        ))}
-      </div> */}
+    <div
+      className={`flex items-center justify-between group font-semibold text-sm outline-none w-full text-gray-700 p-2 ${
+        isSelected ? "bg-gray-50 border-l-4 border-l-blue-500" : ""
+      }`}
+    >
+      {title}
     </div>
   );
 };
 
 // Main component
 async function Sidebar() {
-  const { blobs, folders } = await list({ prefix: "temos/", mode: "folded" });
+  const { blobs } = await list({ prefix: "temos/", mode: "folded" });
 
-  const temoDetails = blobs?.find((blob) => {
-    blob.pathname.endsWith("temos.json");
-    return blob.url;
-  }) || {
-    url: "",
-  };
+  const temoDetails = blobs
+    ?.sort((a: any, b: any) => {
+      return b?.uploadedAt - a?.uploadedAt;
+    })
+    ?.find((blob: any) => {
+      blob.pathname.endsWith("temos.json");
+      return blob.url;
+    });
 
   const allTemos = temoDetails?.url
     ? await fetch(temoDetails?.url).then((res) => res.json())
     : [];
 
-  const temos = folders.map((folder) => ({
-    id: folder,
-    name: folder?.split("/")[1],
-  }));
-
   const publishedTemos = allTemos?.filter((temo: any) => temo?.isPublished);
 
   return (
-    <nav className="h-full bg-white relative flex flex-col border-r border-gray-100 overflow-y-auto">
-      <div className="overflow-y-auto flex-grow">
-        <div className="flex w-full mx-auto px-3 pt-6 pb-8">
-          <div className="flex flex-col w-full h-full text-gray-900 text-xl overflow-y-auto mb-24">
-            {publishedTemos?.map((temo: any) => (
-              <Link href={`/${temo?.sessionId}`} key={temo?.id}>
-                <MenuSection
-                  title={temo?.name}
-                  items={[{ title: temo?.name, icon: "", description: "" }]}
-                />
-              </Link>
-            ))}
-          </div>
-        </div>
-      </div>
+    <nav className="h-full bg-white relative flex flex-col border-r border-gray-100 overflow-y-auto space-y-2">
+      {publishedTemos?.map((temo: any) => (
+        <Link href={`/${temo?.sessionId}`} key={temo?.id}>
+          <MenuSection
+            title={temo?.title}
+            items={[{ title: temo?.title, icon: "", description: "" }]}
+          />
+        </Link>
+      ))}
     </nav>
   );
 }
--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -5,7 +5,9 @@ const TopBar = () => {
   return (
     <div className="z-30 flex top-0 left-0 right-0 items-center justify-between border-b border-gray-100 h-16  bg-white px-4 sm:px-6 lg:px-8 ">
       <div className="flex-1 min-w-0">
-        <p className="text-lg font-semibold hidden sm:block">Temo</p>
+        <Link href="/">
+          <p className="text-lg font-semibold hidden sm:block">Temo</p>
+        </Link>
       </div>
       <div className="flex mt-0 lg:ml-4"></div>
     </div>
