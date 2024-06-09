659ec3847d8eba8d7fdfe2a3a36cca4548c3339f
himanshu-plabs <h@plotlabs.com>
2024-06-09 10:37:27 +0530

    Refactor

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,60 +1,8 @@
 import TemoPlayer from "@/components/TemoPlayer";
-import { list } from "@vercel/blob";
 import ReactMarkdown from "react-markdown";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import { visit } from "unist-util-visit";
-
-const fetchArticle = async (url: string) => {
-  if (!url) {
-    return "";
-  }
-  const response = await fetch(url);
-
-  const text = await response.text();
-  return text;
-};
-
-const fetchFilePaths = async (url: string) => {
-  const { blobs } = await list({
-    prefix: `temos/${decodeURIComponent(url)}`,
-  });
-  let temo: any = {
-    id: url,
-    eventsFileUrl: null,
-    articles: [],
-  };
-  blobs
-    ?.sort((a: any, b: any) => a.uploadedAt - b.uploadedAt)
-    .forEach((blob: any) => {
-      if (blob.pathname.endsWith("events.json")) {
-        temo.eventsFileUrl = blob.url;
-      }
-
-      if (blob.pathname.endsWith(".md")) {
-        let lang = blob.pathname.split("/").pop()?.split(".")[0];
-        temo.articles.push({
-          lang,
-          url: blob.url,
-        });
-      }
-    });
-  return temo;
-};
-
-const extractHeadings = (markdown: string) => {
-  const tree = unified().use(remarkParse).parse(markdown);
-  const headings: { text: string; id: string }[] = [];
-
-  visit(tree, "heading", (node: any) => {
-    const text = node.children.map((child: any) => child.value).join("");
-    const id = text.toLowerCase().replace(/\s+/g, "-");
-    headings.push({ text, id });
-  });
-
-  return headings;
-};
+import { fetchFilePaths, fetchArticle } from "../../lib/fetchArticles";
+import { extractHeadings } from "../../lib/extractHeadings";
 
 export default async function Temo({
   params,
@@ -63,7 +11,7 @@ export default async function Temo({
 }): Promise<JSX.Element> {
   let temosWithPaths = await fetchFilePaths(params.id);
   let article = await fetchArticle(temosWithPaths?.articles[0]?.url);
-  // const headings = extractHeadings(article);
+  const headings = extractHeadings(article);
   return (
     <Tabs
       orientation="vertical"
